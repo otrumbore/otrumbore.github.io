ee00b5e76c2f9a0ee63f4291b7b6c242bbb05bb4
Odnel <otrumbore@gmail.com>
2023-11-11 11:23:09 -0500

    updated social links

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -7,10 +7,10 @@ const SocialIcons = () => {
     <div className='hidden lg:flex fixed flex-col top-[35%] left-0 text-xl'>
         <ul className=''>
             <li className='w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0a66c2] p-4 pl-6 rounded-md text-white'>
-                <a href="/" className='flex justify-between items-center w-full '>LinkedIn <FaLinkedin size={30}/></a>
+                <a href="https://www.linkedin.com/in/odnel-trumbore" target="_blank" className='flex justify-between items-center w-full '>LinkedIn <FaLinkedin size={30}/></a>
             </li>
             <li className='w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333] p-4 pl-6 rounded-md text-white'>
-                <a href="https://github.com/otrumbore/" className='flex justify-between items-center w-full '>GitHub <FaGithub size={30} /></a>
+                <a href="https://github.com/otrumbore/" target="_blank" className='flex justify-between items-center w-full '>GitHub <FaGithub size={30} /></a>
             </li>
             <li className='w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#98ff98] p-4 pl-6 rounded-md text-black'>
                 <a href="/" className='flex justify-between items-center w-full '>E-Mail <HiOutlineMail size={30} /></a>
