1e8e762a7dafc8f077d75623b6165ef7ff26b1df
Odnel <otrumbore@gmail.com>
2023-11-13 12:09:37 -0500

    fixed about page layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,20 +4,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-screen'>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-[100%] grid grid-cols-2 gap-8'>
-          <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl md:text-5xl lg:text-6xl font-bold inline border-b-4 border-b-[#37cdbe]'>About</p>
+    <div name="about" className='w-full h-screen mt-[30%] lg:mt-0'>
+      <div className='flex flex-col items-center justify-center px-8 lg:space-x-5'>
+        <div className='flex flex-row max-w-[1000px] w-[100%]'>
+          <div className='w-[100%] lg:w-5/12 pb-10 lg:text-right'>
+            <p className='text-4xl md:text-5xl lg:text-6xl font-bold inline border-b-4 border-b-[#000080] dark:border-b-[#37cdbe]'>About</p>
           </div>
-          <div></div>
+          <div className='hidden w-7/12'></div>
         </div>
-        <div className='max-w-[1000px] w-[100%] grid grid-cols-2 gap-8'>
-          <div className='sm:text-right text-3xl lg:text-5xl font-bold'>
+        <div className='max-w-[1000px] w-[100%] flex flex-col lg:flex-row space-y-5 lg:space-x-5 lg:space-y-0'>
+          <div className='lg:w-5/12 lg:text-right text-2xl md:text-3xl lg:text-4xl font-bold'>
             <p>Hi I'm Odnel, please take a look around.</p>
           </div>
-          <div>
-            <p>About me here!!</p>
+          <div className='lg:w-7/12 text-lg'>
+            <p>
+            Highly-motivated employee with desire to take on new challenges. Strong 
+          worth ethic, adaptability and exceptional interpersonal skills. Adept at working effectively unsupervised and quickly mastering new skills. 
+          Expert in transferable skills such as coding, data analysis, or other technical skills with a strong commitment to learn and master additional 
+          skills through certifications and course studies. 
+            </p>
           </div>
         </div>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
 
       <div className='max-w-[1100px] mx-auto px-8 flex flex-col lg:flex-row lg:space-x-5 items-center justify-center h-full'>
         <div className='flex-auto'>
-        <p className='dark:text-[#37cdbe] text-[#031b28] text-lg md:text-xl'>Hi there, I am</p>
-        <h1 className='text-4xl md:text-5xl lg:text-6xl  font-bold text-[#000080] dark:text-[#ccd6f6]'>Odnel Trumbore</h1>
-        <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold dark:text-[#8892b0]'>I'm a Frontend Devloper</h2>
+        <p className='dark:text-[#37cdbe] text-[#000080] text-lg md:text-xl'>Hi there, I am</p>
+        <h1 className='text-4xl md:text-5xl lg:text-6xl  font-bold text-gray-800 dark:text-[#ccd6f6]'>Odnel Trumbore</h1>
+        <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold dark:text-[#8892b0]'>A Frontend Devloper</h2>
         <p className='dark:text-[#8892b0] py-4 max-w-[700px] text-lg md:text-xl'>Highly-motivated employee with desire to take on new challenges. Strong 
           worth ethic, adaptability and exceptional interpersonal skills. Adept at working effectively unsupervised and quickly mastering new skills. 
           Expert in transferable skills such as coding, data analysis, or other technical skills with a strong commitment to learn and master additional 
